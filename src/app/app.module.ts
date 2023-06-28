5541f3f3814b55b45330e9a9b718d13ef8f3d75d
IanZeta64 <ianpf7@gmail.com>
2023-06-27 22:12:57 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { InfoComponent } from './components/info/info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SkillsComponent,
+    InfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
